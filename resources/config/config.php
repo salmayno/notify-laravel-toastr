a7c380943bd1b5a1e2de4c2dd95eab0572ad9d2e
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-11-27 15:30:51 +0100

    update jquery

--- resources/config/config.php
+++ resources/config/config.php
@@ -2,7 +2,7 @@
 
 return array(
     'scripts' => array(
-        'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js',
     ),
     'styles' => array(
